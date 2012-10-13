dbb3a59ea6b1305bffad738f981c587712fe203a
Mark Miyashita <negativetwelve@gmail.com>
2012-10-13 12:42:55 -0700

    fixed graphs

---
+++ db/migrate/20121013172710_remove_duration_f_from_events.rb
@@ -0,0 +1,7 @@
+class RemoveDurationFFromEvents < ActiveRecord::Migration
+  def up
+  end
+
+  def down
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121013141421) do
+ActiveRecord::Schema.define(:version => 20121013172915) do
 
   create_table "events", :force => true do |t|
     t.text     "word"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(:version => 20121013141421) do
     t.datetime "updated_at", :null => false
     t.text     "event_type"
     t.text     "window"
+    t.text     "duration"
   end
 
   create_table "microposts", :force => true do |t|
