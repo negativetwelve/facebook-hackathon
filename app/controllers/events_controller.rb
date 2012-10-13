046233840cc1133aba1c54d72c1ea07ba72f67da
Mark Miyashita <negativetwelve@gmail.com>
2012-10-13 10:19:51 -0700

    3rd graph hopefully working:'

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,11 +3,11 @@ class EventsController < ApplicationController
   def show
     @e = Event.new
     if params[:event_id] == "1"
-      @data = @e.word_frequency(Event.all)
+      @data = @e.word_frequency(Event.where(event_type: "WORD"))
     elsif params[:event_id] == "2"
       @data = @e.click_position(Event.where(event_type: "Mouse"))
     elsif params[:event_id] == "3"
-      
+      @data = @e.time_track(Event.where(event_type: "Screen"))
     end
   end
   
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,35 +1,49 @@
 class Event < ActiveRecord::Base
-  attr_accessible :date, :time, :word
+  attr_accessible :date, :time, :word, :window
   
   def word_frequency(events)
-    d = DateTime.parse(DateTime.now.to_s)
-    @data = []
+    data = []
     events.each do |e|
       date = Time.parse(e.date.to_s + " " + e.time.to_s)-7.hours
       t = (Time.now - date).to_i/60
       if t < 60
-        if @data[t].nil?
-          @data[t] = 1
+        if data[t].nil?
+          data[t] = 1
         else
-          @data[t] += 1
+          data[t] += 1
         end
       end
     end
-    return @data
+    return data
   end
   
   def click_position(events)
-    @data = []
+    data = []
     (0..9).each do |i|
       (0..9).each do |j|
-        @data[i*10+j] = [i*128, j*80, 0]
+        data[i*10+j] = [i*128, j*80, 0]
       end
     end
     events.each do |e|
       pos = e.word.split(",")
-      @data[pos[0].to_i/128*10 + pos[1].to_i/80][2] += 1
+      data[pos[0].to_i/128*10 + pos[1].to_i/80][2] += 1
     end
-    return @data
+    return data
+  end
+  
+  def time_track(events)
+    apps = []
+    time = []
+    data = [apps, time]
+    events.each do |e|
+      if data[0].include?(e.window)
+        data[1][data[0].index(e.window)] += e.duration
+      else
+        data[0].append(e.window)
+        data[1].append(e.duration)
+      end
+    end
+    return data
   end
   
 end
